bfb75fae2a8ce789253ba51340808015423a7987
Arpan_Dey <arpan2007dey@gmail.com>
2025-08-29 23:08:46 +0530

    Add files via upload

---
+++ main4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int num1,num2;
+	printf("enter value for first number: ");
+	scanf("%d",&num1);
+	printf("enter value for second number: ");
+	scanf("%d",&num2);
+	printf("The sum of the given two numbers are: %d",num1+num2);
+	
+	
+	return 0;
+}
